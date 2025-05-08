a31239a80a2b88e792f49e829a032f56ef2550a5
Osmar Yair Ramirez Perez <ing.osmardv@gmail.com>
2025-05-07 21:43:33 -0600

    Add click handler to logo for navigation to welcome page and update styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
 import { Routes, Route } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import Welcome from "./components/Welcome/Welcome";
 import gapsiLogo from "./assets/logo.png";
 import Providers from "./components/Providers/Providers";
 import "./App.css";
 
 function App() {
+  const navigate = useNavigate();
+
+  const handleWelcome = () => {
+    navigate("/");
+  };
+
   return (
     <div className="app">
       <div className="con">
         <header className="header">
-          <img src={gapsiLogo} alt="Gapsi Logo" className="logo" />
+          <img src={gapsiLogo} alt="Gapsi Logo" className="logo" onClick={handleWelcome} />
           <h1>e-Commerce Gapsi</h1>
         </header>
         <Routes>
