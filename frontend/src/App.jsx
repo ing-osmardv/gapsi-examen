fd1b6b037e90762a27c937917f792e2f6dd5921d
Osmar Yair Ramirez Perez <ing.osmardv@gmail.com>
2025-05-07 20:40:03 -0600

    Add React Router for navigation and update App component structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
-import "./App.css";
+import { Routes, Route } from 'react-router-dom';
+import Welcome from "./components/Welcome/Welcome";
 
 function App() {
-
-  return <></>;
+  return (
+    <div className="app">
+      <Routes>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
